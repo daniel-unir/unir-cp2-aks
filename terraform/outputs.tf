9a7f7a979d3b670aa1382732f426b945e7e96cf4
daniel-unir <daniel.gutierrez149@comunidadunir.net>
2025-07-24 12:00:52 +0200

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -10,3 +10,11 @@ output "acr_pwd" {
   value       = azurerm_container_registry.acr-unir-casopractico2.admin_password
   sensitive   = true
 }
+output "client_certificate" {
+  value     = azurerm_kubernetes_cluster.aks-unir-casopractico2.kube_config[0].client_certificate
+  sensitive = true
+}
+output "kube_config" {
+  value = azurerm_kubernetes_cluster.aks-unir-casopractico2.kube_config_raw
+  sensitive = true
+}
